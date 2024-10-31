5438912167db0707e22c34acf503b7534f3411f4
Ankur Kumar <ankuralpha404@gmail.com>
2024-10-31 17:10:52 +0530

    add all basic requirements for a properly working timer

--- main.py
+++ main.py
@@ -10,31 +10,67 @@ class gui(tk.Tk):
         self.minsize(300, 300)
         self.maxsize(900, 900)
         self.title("Pomodoro app")
-        self.resetter = False
+        self.time_breaker = False
         self.bigfont_cond = "start"
 
     def resetting_process(self):
-        self.resetter = True
+        self.time_breaker = True
+        self.dvar.set(time.strftime("%H:%M:%S", time.gmtime(0)))
+        self.disp.update()
+        self.bigfntframe.destroy()
+        self.the_main_process()
+
+    def resetting_pomo_count(self):
+        self.bvar = 0
+        self.tvar = 0
+        self.svar.set(f"Pomos : {self.tvar} Breaks : {self.bvar} resetted!!")
+
+    def play(self):
+        if self.time_breaker is True:
+            self.time_breaker = False
+            self.start_timer()
+        else:
+            tmsg.showinfo("Warning", "The timer is already going on!!")
+
+    def pause(self):
+        if self.time_breaker is False:
+            self.time_breaker = True
+        else:
+            tmsg.showinfo("Warning", "The timer is already paused!!")
 
     def menubar(self):
+        def about_software():
+            t = """This is a simple pomodoro timer software\n being distributed under MIT Licence\n\n source : \n https://github.com/AnkurAlpha/pomodoro-timer-project"""
+            tmsg.showinfo("about software", t)
+
+        def about_author():
+            t = """The author is AnkurAlpha(Ankur Kumar)\n an engineering student at VIT Chennai (2024-2028)\n persuing his B.Tech degree
+
+            link : https://github.com/AnkurAlpha"""
+            tmsg.showinfo("about_author", t)
+
         mainmenu = tk.Menu(self)
 
         # timer option
         m1 = tk.Menu(mainmenu, tearoff=0)
+        m1.add_command(label="play", command=self.play)
+        m1.add_command(label="pause", command=self.pause)
+        m1.add_separator()
         m1.add_command(label="reset", command=self.resetting_process)
-        # m1.add_command(label="stop")
+        m1.add_command(label="reset count", command=self.resetting_pomo_count)
         mainmenu.add_cascade(label="timer", menu=m1)
 
         # info option
         m2 = tk.Menu(mainmenu, tearoff=0)
-        m2.add_command(label="about")
+        m2.add_command(label="about software", command=about_software)
+        m2.add_command(label="about author", command=about_author)
         mainmenu.add_cascade(label="about", menu=m2)
 
         # exit
         def quitapp():
             a = tmsg.askyesno("Quit", "Do you want to quit the application ?")
             if a:
-                self.resetter = True
+                self.time_breaker = True
                 self.disp.destroy()
                 self.destroy()
 
@@ -90,6 +126,7 @@ class gui(tk.Tk):
         self.dvar.set(time.strftime("%H:%M:%S", time.gmtime(secs)))
 
     def start_timer(self):
+        self.time_breaker = False
         self.svar.set(f"Pomos : {self.tvar} Breaks : {self.bvar}")
         while True:
             self.bigfont_cond = "no_break"
@@ -97,13 +134,11 @@ class gui(tk.Tk):
             for i in range(self.counttime*60):
                 self.dvar.set(time.strftime("%H:%M:%S", time.gmtime(i)))
                 self.disp.update()
-                if self.resetter is True:
-                    self.dvar.set(time.strftime("%H:%M:%S", time.gmtime(0)))
-                    self.disp.update()
+                if self.time_breaker is True:
                     break
                 time.sleep(1)
             self.changesbar("time")
-            if self.resetter is True:
+            if self.time_breaker is True:
                 break
             repeater = tmsg.askyesno(
                 "Time Over", "Time is over. Do you want break ?")
@@ -115,46 +150,43 @@ class gui(tk.Tk):
                         "%H:%M:%S", time.gmtime(i)))
                     self.disp.update()
                     time.sleep(1)
-                    if self.resetter is True:
-                        self.dvar.set(time.strftime(
-                            "%H:%M:%S", time.gmtime(0)))
-                        self.disp.update()
+                    if self.time_breaker is True:
                         break
                 self.changesbar("break")
-            if self.resetter is True:
+            if self.time_breaker is True:
                 break
             tmsg.askyesno(
                 "Break Over", "Break is over")
 
-    def the_main_process(self):
+    def ignite(self):
+        self.counttime = self.timeslider.get()
+        self.countbreak = self.breakslider.get()
+        self.frm1.destroy()
+        self.frm2.destroy()
+        self.frm3.destroy()
+        self.bigfont_create()
+        self.start_timer()
 
-        def ignite():
-            self.counttime = timeslider.get()
-            self.countbreak = breakslider.get()
-            self.frm1.destroy()
-            self.frm2.destroy()
-            self.frm3.destroy()
-            self.bigfont_create()
-            self.start_timer()
+    def the_main_process(self):
 
         self.frm1 = tk.Frame(self)
         self.frm1.pack(fill="x")
-        timeslider = tk.Scale(self.frm1, from_=0, to=120,
-                              orient="horizontal", tickinterval=60)
-        timeslider.pack(side="right")
+        self.timeslider = tk.Scale(self.frm1, from_=0, to=120,
+                                   orient="horizontal", tickinterval=60)
+        self.timeslider.pack(side="right")
         timemsg = tk.Label(self.frm1, text="Pomo time : ")
         timemsg.pack(side="left")
         self.frm2 = tk.Frame(self)
         self.frm2.pack(fill="x")
-        breakslider = tk.Scale(self.frm2, from_=0, to=30,
-                               orient="horizontal", tickinterval=10)
-        breakslider.pack(side="right")
+        self.breakslider = tk.Scale(self.frm2, from_=0, to=30,
+                                    orient="horizontal", tickinterval=10)
+        self.breakslider.pack(side="right")
         breakmsg = tk.Label(self.frm2, text="Break time : ")
         breakmsg.pack(side="left")
         self.frm3 = tk.Frame(self, bg="red")
         self.frm3.pack()
         but1 = tk.Button(self.frm3, relief="sunken", border=5,
-                         text="start", command=ignite)
+                         text="start", command=self.ignite)
         but1.pack()
 
     def bigfont_create(self):
